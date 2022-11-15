8a30e5356112e9905c21fc4d2f837ba00faabd14
Sabina Bayramova <sb.sabina00@gmail.com>
2022-11-15 12:48:32 +0300

    Add tiny changes in create_dataset script

--- src/text_generator/create_dataset.py
+++ src/text_generator/create_dataset.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import click
 from corus import load_mokoron
+from numpy.random import default_rng
 from razdel import sentenize
 
 
@@ -29,19 +30,39 @@ from razdel import sentenize
     show_default=True,
     help="Number of records to include in dataset.",
 )
-def create_dataset(input_dir: Path, output_dir: Path, size: int) -> None:
+@click.option(
+    "--train-split",
+    type=click.FloatRange(min=0.0, max=1.0),
+    default=0.8,
+    show_default=True,
+    help="Proportion of the dataset to include in the train split.",
+)
+@click.option("--seed", type=int, default=1111, show_default=True, help="Random seed.")
+def create_dataset(
+    input_dir: Path, output_dir: Path, size: int, train_split: float, seed: int
+) -> None:
     click.echo("Loading dataset...\n")
     records = load_mokoron(input_dir)
+
     sentences = []
     for record in islice(records, size):
         text = record.text
         text = text.replace("\n", " ")
         for sentence in sentenize(text):
             sentences.append(sentence.text)
-    click.echo(f"Number of sentences in train dataset: {size}")
-    click.echo(f"Number of sentences in valid dataset: {len(sentences) - size}\n")
-    train_corpus = "\n".join(sentences[:size])
-    valid_corpus = "\n".join(sentences[size:])
+
+    rng = default_rng(seed)
+    rng.shuffle(sentences)
+
+    TRAIN_SPLIT = int(len(sentences) * train_split)
+    train_corpus = "\n".join(sentences[:TRAIN_SPLIT])
+    valid_corpus = "\n".join(sentences[TRAIN_SPLIT:])
+
+    click.echo(f"Number of sentences in train dataset: {TRAIN_SPLIT}")
+    click.echo(
+        f"Number of sentences in valid dataset: {len(sentences) - TRAIN_SPLIT}\n"
+    )
+
     with open(os.path.join(output_dir, "train.txt"), "w", encoding="utf8") as file:
         file.write(train_corpus)
     with open(os.path.join(output_dir, "valid.txt"), "w", encoding="utf8") as file:
